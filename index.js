04337459cfe08e7d799e7e3b7faa85bd33fc4ab1
Kyle Kemp <kyle@seiyria.com>
2019-02-26 07:55:32 -0600

    add awk 9/10 icons

--- index.js
+++ index.js
@@ -132,6 +132,11 @@ const char = (msg, args, { region, desc }) => {
     return;
   }
 
+  let awk = '';
+  if(charData.awakened) {
+    awk = getEmoji(`sbrAwk${charData.awakened === true ? 10 : 9}`);
+  }
+
   const embed = new Discord.RichEmbed()
     .setAuthor(`${charData.name} [${charData.cat.toUpperCase()}]`, `${ASSET_URL}/icons/charclass/class-${charData.type}.png`)
     .setDescription(desc ? charData.notes.substring(0, 2048) : '')
@@ -140,7 +145,7 @@ const char = (msg, args, { region, desc }) => {
     .setURL(`https://anamnesiac.seiyria.com/characters?region=${charData.cat}&char=${encodeURI(charData.name)}`)
     .setFooter(ref[0][0] === 1 ? '' : `Sorry, I could not find an exact match for "${args}". This'll have to do, 'kay?`);
 
-  embed.addField('About', `${getEmoji(`sbrRarity${charData.star}`)} ${charData.ace ? 'ACE' : ''} ${charData.limited ? 'Limited' : ''} - ${weaponHash[charData.weapon]} User`);
+  embed.addField('About', `${getEmoji(`sbrRarity${charData.star}`)} ${awk} ${charData.ace ? 'ACE' : ''} ${charData.limited ? 'Limited' : ''} - ${weaponHash[charData.weapon]} User`);
 
   charData.talents.forEach(tal => {
     embed.addField(`Talent: ${tal.name}`, tal.effects.map(x => `* ${x.desc} ${x.all ? `(All ${x.all === true ? 'Party' : x.all})` : ''}`).join('\n'));
